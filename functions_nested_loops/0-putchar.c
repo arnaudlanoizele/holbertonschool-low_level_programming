2e772234544c42c1c107a4a8ba75ed4f1385aaf9
arnaudlanoizele <10476@holbertonstudents.com>
2025-02-21 13:15:31 +0100

    Write a program... by a new line

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - putchar stdio
 *
@@ -7,15 +7,6 @@
 
 int main(void)
 {
-	_putchar(72);
-	_putchar(111);
-	_putchar(108);
-	_putchar(101);
-	_putchar(101);
-	_putchar(114);
-	_putchar(116);
-	_putchar(111);
-	_putchar(110);
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
