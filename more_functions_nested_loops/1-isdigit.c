aaa29f85cbe909486dccfadb551ed396e4a34d00
arnaudlanoizele <10476@holbertonstudents.com>
2025-02-24 11:35:24 +0100

    Writz a function that checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - a function that checks for a digit (0 through 9)
+ * @ch: input to check for
+ * Return: 1 if ch is a digit else 0
+ */
+
+int _isdigit(int ch)
+{
+	int result = 0;
+
+	if (ch >= 48 && ch <= 57)
+	{
+		result = 1;
+	}
+	return (result);
+}
