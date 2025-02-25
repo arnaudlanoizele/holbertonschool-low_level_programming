d950367ac63de449abf9d6ec0a5780d6c3794f75
arnaudlanoizele <10476@holbertonstudents.com>
2025-02-25 19:20:36 +0100

    Write a function that copies... the buffer pointed to by dest

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcpy - copies the string pointed to by src
+* including the terminal null byte (\0)
+* to the buffer pointed to by dest
+* @src: pointer to string
+* @dest: buffer to copy the string to
+* Return: buffer dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int counter = 0;
+
+	while (1)
+	{
+		dest[counter] = src[counter];
+		if (src[counter] == '\0')
+		{
+			break;
+		}
+		counter++;
+	}
+
+	return (dest);
+}
