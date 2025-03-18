a90fa3b988938e20ebf3736a6eaf7e6607c1afe2
arnaudlanoizele <10476@holbertonstudents.com>
2025-03-18 14:21:35 +0100

    Sup a desciption

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,12 @@
-/**
-* File: 0-print_name.
-* Auth: Arnaud Lanoizele
-*/
-
 #include "function_pointers.h"
 #include <stdio.h>
 
 /**
- * print_name - Prints a name.
+ * print_name - Calls a function to print a name
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
- * Return: Nothing
+	*
+	* Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
+/*
+ * File: function_pointers.h
+ * AUth: Arnaud Lanoizele
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
 
-int _putchar(char c);
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
