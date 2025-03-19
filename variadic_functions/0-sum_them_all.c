085f81894e43d3e25da938f6bd4dfbce317cc474
arnaudlanoizele <10476@holbertonstudents.com>
2025-03-19 13:17:26 +0100

    Creation of files main sum_theme_all and variadic_functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - Returns the sum of all its parameters
+* @n: The numbers of paramters passed to the function
+* @...: A variable number of parameters to calculate the sum of
+*
+* Return: If n == 0 - 0
+*			Otherwise - The sum of all parameters
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index, sum = 0;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		sum +=  va_arg(nums, int);
+	}
+
+	va_end(nums);
+
+	return (sum);
+}
