6538e80eb5e5b58727ef75895bcff27a9ab0ff3e
arnaudlanoizele <10476@holbertonstudents.com>
2025-04-02 13:41:17 +0200

    file 8-delete_dnodeint

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,5 +12,32 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 {
+	dlistint_t *tmp = *head;
 
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
 }
