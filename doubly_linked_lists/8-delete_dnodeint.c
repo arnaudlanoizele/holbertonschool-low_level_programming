f122000557c7bf1c38b387de0bd4dc92465b00fa
arnaudlanoizele <10476@holbertonstudents.com>
2025-04-02 13:44:16 +0200

    adds linea 38 next

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		tmp->next = tmp->next;
 		if (tmp->next != NULL)
-			tmp->prev = tmp->prev;
+			tmp->next->prev = tmp->prev;
 	}
 
 	free(tmp);
